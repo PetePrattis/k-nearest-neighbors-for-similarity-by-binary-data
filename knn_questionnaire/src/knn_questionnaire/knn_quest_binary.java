babe1005a869c30ae948e1fbf324cb1813932fd5
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-12-17 12:39:28 +0200

    Update knn_quest_binary.java

--- knn_questionnaire/src/knn_questionnaire/knn_quest_binary.java
+++ knn_questionnaire/src/knn_questionnaire/knn_quest_binary.java
@@ -61,14 +61,13 @@ public class knn_quest_binary {
 			ArrayList<Double> ssim = new ArrayList<Double>(); //sorted 
 
 			boolean hamming = false;
-			for(int i=0; i<quest.length; i++) {	//for all users (21)
-				//HAMMING AND COSINE GIA BINARY DATASET
+			for(int i=0; i<quest.length; i++) {
+				//HAMMING and COSINE are best for binary dataset
 				//hammingDist(quest, num, i, user, sim, ssim);
-				//hamming = true;
 				cosineSimilarity(quest,i,num,user,sim,ssim);
 			}
 			
-			Collections.sort(ssim); //apo to mikrotero sto megalutero
+			Collections.sort(ssim);
 			
 			if (!hamming)
 			{
@@ -78,10 +77,9 @@ public class knn_quest_binary {
 			
 		    int remove = ssim.size() - k;
 		    for (int i =0; i<remove;i++) {
-		    	ssim.remove(ssim.size()-1); //afairw tosous xristes wste na meinoun k similar
+		    	ssim.remove(ssim.size()-1); //remove locations sot hat remain only k similar
 		    }
 		    
-		    //an enas xristis ehei to similarity enos similar xristi, tote valton stin lista suser. auto sumbainei mia fora-gia ena mono xristi (once metavliti)
 		    boolean once = true;
 		    for (int i=0; i<ssim.size(); i++) {
 		    	for (int j=0; j<sim.size(); j++) {
@@ -98,48 +96,18 @@ public class knn_quest_binary {
 		    System.out.println(suser);
 		    System.out.println(ssim);
 		    
-		    /*double[] recommended = new double[quest[0].length]; //vathmologies oles gia ton user
-		    double weightedSum = 0; 
-		    double similaritySum = 0; //osoi users einai pio konta tote ypologizetai 
-		    for(int i=0; i<quest[0].length; i++) {
-		    	for(int j=0; j<quest.length-1; j++) {
-		    		//if(ssim.contains(sim.get(j))) {
-		    		if(suser.contains(user.get(j))) {
-		    			if(quest[num][i] == 0) {
-		    				weightedSum += quest[j][i]*sim.get(j);
-		    				//System.out.println(user.get(j)+","+sim.get(j));
-		    				similaritySum += sim.get(j);
-		    			//}
-		    		}
-		    		
-		    	}
-		    	if(weightedSum == 0) {
-		    		double d = quest[num][i];
-		    		recommended[i] = d;
-		    	}
-		    	else {
-			    	weightedSum /= similaritySum; //to athroisma twn similarities
-			    	recommended[i] = weightedSum;
-		    	}
-		    	weightedSum=0;
-		    	similaritySum=0;
-		    }
-		    for(double d: recommended)
-		    	System.out.println(d);
-		    
-		    
-	    }*/
-	}
+		   
+		}
 		
 		public static void hammingDist (int [][] quest, int num, int i,ArrayList<Integer> user,ArrayList<Double> sim,ArrayList<Double> ssim) {
 		
 			//num = user to ignore
-			//i user to observe
+			//i = user to observe
 			double distance = 0.0;
 			boolean flag = true;
-			for (int j=0; j <quest[0].length;j++) //gia to ekastote feature
+			for (int j=0; j <quest[0].length;j++) //for each location feature / characteristic
 			{
-				if (i != num) { //an o observed user DEN einai autos pou sygkrinw me allous
+				if (i != num) { 
 					flag = true;
 					if (quest[num][j] != quest[i][j])
 						distance++;
@@ -151,7 +119,7 @@ public class knn_quest_binary {
 			}
 			if (flag)
 			{
-				System.out.println("user: "+num+" has similarity to user: "+ i+" of value "+distance); //similarity me tous ypoloipous users
+				System.out.println("location: "+num+" has similarity to user's preferred location features : "+ i+" of value "+distance); //similarity to rest locations
 				user.add(i);
 				sim.add(distance);
 				ssim.add(distance);
@@ -161,26 +129,24 @@ public class knn_quest_binary {
 		public static void euclideanDist(int [][] quest,int num, int i, ArrayList<Integer> user,ArrayList<Double> sim,ArrayList<Double> ssim) {
  			double sumSquares = 0;
 			boolean flag = true;
-			for(int j=0; j<quest[0].length; j++) { //for eacch topothesia
-				if(i != num) {// && quest[num][j]!=0 && quest[i][j]!=0) { //an o pros eksetasi user den einai o user pou kanoume recommend (i!=num) & gia ta diathesima 
-						//quest pou ehoume apo ton user pou provlepoume alla kai tous ypoloipous users pou eksetazoume
-						//euclidean
+			for(int j=0; j<quest[0].length; j++) {
+				if(i != num) {//euclidean
 					double diff = 0;
 					diff = quest[num][j] - quest[i][j]; 
 					sumSquares += diff*diff; //to be eucledian
 					flag = true;
 				}
-				else if (i == num) //an o xristis pros eksetasi einai o user pou theloume provlepsi
+				else if (i == num) 
 					flag = false;
 			}
-			if(flag) { //an kaname SUGKRISI me olous ektos apo ton xristi pou epizitoume PROVLEPSI
+			if(flag) {
 				double d = Math.sqrt(sumSquares); //eucledian dist
 				double similarity = 1/d;
 				if (Double.isInfinite(similarity)) {
-					similarity = 1; //einai EXACT SAME
+					similarity = 1;
 				}
 			
-				System.out.println("user: "+num+" has similarity to user: "+ i+" of value "+similarity); //similarity me tous ypoloipous users
+				System.out.println("location: "+num+" has similarity to user's preferred location features : "+ i+" of value "+distance); //similarity to rest locations
 				user.add(i);
 				sim.add(similarity);
 				ssim.add(similarity);
@@ -209,7 +175,7 @@ public class knn_quest_binary {
 			}
 			else {
 				similarity = sumProduct / (Math.sqrt(sumASq) * Math.sqrt(sumBSq));
-				System.out.println("user: "+num+" has similarity to user: "+ i+" of value "+similarity); //similarity me tous ypoloipous users
+				System.out.println("location: "+num+" has similarity to user's preferred location features : "+ i+" of value "+similarity); //similarity to rest locations
 			}
 			
 			
@@ -243,37 +209,11 @@ public class knn_quest_binary {
 			if (flag)
 			{
 				total = res + res1;
-				System.out.println("user: "+num+" has similarity to user: "+ i+" of value "+ total); //similarity me tous ypoloipous users
+				System.out.println("location: "+num+" has similarity to user's preferred location features : "+ i+" of value "+total); //similarity to rest locations
 				user.add(i);
 				sim.add(total);
 				ssim.add(total);
 			}
 		}
-		
-		/*public static void manhattanDistance (int [][] quest, int i, int num,ArrayList<Integer> user,ArrayList<Double> sim,ArrayList<Double> ssim )
-		{
-			int similarity =0;
-			boolean flag = true;
-			for (int j=0;j<quest[0].length;j++)
-			{
-				if(i != num) {// && quest[num][j]!=0 && quest[num][i] != 0 && quest[i][j]!=0 && quest[i][i] !=0) {
-					flag = true;
-					similarity += Math.abs(quest[num][j] - quest[num][j+1]) + Math.abs(quest[i][j] - quest[i][j+1]);
-					//similarity= Math.abs(quest[num][i]-quest[num][j]) +
-						//Math.abs(quest[i][i] - quest[i][j]);
-				}
-				else
-					flag = false;
-				
-			}
-			if (flag) {
-				System.out.println("user: "+num+" has similarity to user: "+ i+" of value "+similarity); //similarity me tous ypoloipous users
-				user.add(i);
-				sim.add((double) similarity);
-				ssim.add((double) similarity);
-			}
-		}*/
-		
-		
-		
+	
 }
